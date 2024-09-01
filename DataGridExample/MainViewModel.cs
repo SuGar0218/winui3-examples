4d9302f0b1dab25404f33c4d3584622ad0fb8b91
SuGar0218 <122210889+SuGar0218@users.noreply.github.com>
2024-09-01 11:04:01 +0800

    Add files via upload

---
+++ DataGridExample/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridExample {
+    internal class MainViewModel {
+        public class User(string account, string username, string password, bool isActivated = false) {
+            public string Account { get; set; } = account;
+            public string Username { get; set; } = username;
+            public string Password { get; set; } = password;
+            public bool IsActivated { get; set; } = isActivated;
+        }
+
+        internal ObservableCollection<User> Users { get; set; } = [
+            new User("001", "Tom", "t_o_m", true),
+            new User("002", "Jerry", "j"),
+        ];
+
+        internal void AddOne() {
+            var random = new Random();
+            Users.Add(new(random.Next().ToString(), random.Next().ToString(), random.Next().ToString()));
+        }
+
+        internal void RemoveOne() {
+            if (Users.Count == 0) return;
+            Users.RemoveAt(Users.Count - 1);
+        }
+    }
+}
